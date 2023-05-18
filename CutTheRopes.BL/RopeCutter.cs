40464acda1f9f7ce050d1c768392130673ffd827
jesse <jessefortuin850@gmail.com>
2023-05-18 14:01:12 +0200

    Feat: Method implemented

--- CutTheRopes.BL/RopeCutter.cs
+++ CutTheRopes.BL/RopeCutter.cs
@@ -4,9 +4,44 @@
     {
         public int[] CutTheRopes(int[] a)
         {
-            
-            
-            return a;
+            List<int> result = new List<int>();
+
+            int numRopes = a.Length;
+
+            while (numRopes > 0)
+            {
+                result.Add(numRopes);
+
+                int minRopeLength = GetMinRopeLength(a);
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] > 0)
+                    {
+                        a[i] -= minRopeLength;
+                        if (a[i] == 0)
+                        {
+                            numRopes--;
+                        }
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        public static int GetMinRopeLength(int[] ropes)
+        {
+            int minRopeLength = int.MaxValue;
+
+            for (int i = 0; i < ropes.Length; i++)
+            {
+                if (ropes[i] > 0 && ropes[i] < minRopeLength)
+                {
+                    minRopeLength = ropes[i];
+                }
+            }
+            return minRopeLength;
         }
     }
 }
